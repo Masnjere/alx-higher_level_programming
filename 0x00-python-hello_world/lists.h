c0ad5fe9fe67e2fc8cdf0cf6dac4a1405335be74
Masnjere <masnjere@gmail.com>
2024-06-06 10:29:18 -0700

    Linked list cycle

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,17 +4,17 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - single linkd list
- * @n: int
- * @next: pnts to next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: single linkd list node struct
- * for the Holberton School project
+ * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
