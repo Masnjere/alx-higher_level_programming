adb5370cc3bc62449932b9a5c7bdc22718c785ad
Masnjere <masnjere@gmail.com>
2024-08-12 21:55:52 -0700

    my first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a module for my first square"""
+
+class Square:
+    """this represents a square."""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is my solution for the project: 0x06. Python - Classes and Objects
